9924eeecb185f258ae5b78cfc3abf97aa19bba51
Devam Agrawal <dev.devam.a@gmail.com>
2022-08-15 04:07:09 +0530

    Project setup files.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">App</header>
+      <div>
+        <span className="font-bold">test</span>
+        {/* user card 1 */}
+
+        {/* user card 2 */}
+
+        {/* user card 3 */}
+
+        {/* user card 4 */}
+
+        {/* user card 5 */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
