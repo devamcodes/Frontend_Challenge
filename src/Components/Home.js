71dd388d86e89d468276f1e818ce7e4ed36f775f
Kushal <Kushalhemant2003@gmail.com>
2022-08-22 23:45:02 +0530

    Changed design, added some functionality

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,31 @@
-import { Grid, TextField, FormControlLabel, Paper, Checkbox, Button, Snackbar, Alert } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Grid, TextField, FormControlLabel, Paper, Checkbox, Button, Snackbar, Alert, ButtonGroup, InputAdornment, OutlinedInput, IconButton } from "@mui/material";
+import React, { useState } from "react";
 import { useFormik} from "formik";
 import AnotherCard from "./AnotherCard";
 import * as Yup from'yup'
 import {useNavigate} from 'react-router-dom'
+import { Visibility} from '@mui/icons-material'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 
 function Home({allUser,setAllUser}) {
-
-    useEffect(()=>{
-        if(!LogInUser){
-            navigate('/login')
-        }
-    })
 
     const LogInUser=localStorage.getItem('username')
     // localStorage.removeItem('username')
     const navigate=useNavigate()
     
+    const [visibility,setVisibility]=useState(false)
+    const handleshow=()=>{
+      setVisibility(!visibility)
+    }
+
     const [open, setOpen] = useState(false);
 
     const [user, setUser] = useState({
 		id: 0,
 		name: "",
 		password: "",
-		days: { monday: false, tuesday: false, wednesday: false },
+		days: { monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false  },
 	});
 
 	const update = (User) => {
@@ -40,6 +41,10 @@ function Home({allUser,setAllUser}) {
 				tempUser[index].days.monday = user.days.monday;
 				tempUser[index].days.tuesday = user.days.tuesday;
 				tempUser[index].days.wednesday = user.days.wednesday;
+				tempUser[index].days.thursday = user.days.thursday;
+				tempUser[index].days.friday = user.days.friday;
+				tempUser[index].days.saturday = user.days.saturday;
+				tempUser[index].days.sunday = user.days.sunday;
 				break;
 			}
 		}
@@ -53,7 +58,7 @@ function Home({allUser,setAllUser}) {
 				id: 0,
 				name: "",
 				password: "",
-				days: { monday: false, tuesday: false, wednesday: false },
+				days: { monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false },
 			})
       		resetForm()
 		} else {
@@ -63,7 +68,7 @@ function Home({allUser,setAllUser}) {
 				id: 0,
 				name: "",
 				password: "",
-				days: { monday: false, tuesday: false, wednesday: false },
+				days: { monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false },
 			})
       		resetForm()
 		}
@@ -102,6 +107,15 @@ function Home({allUser,setAllUser}) {
         navigate('/login')
     }
 
+    const handleCancelClick = () => {
+      setUser({
+				id: 0,
+				name: "",
+				password: "",
+				days: { monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false },
+			})
+    }
+
   return (
     <div>
         <h1 style={{ fontSize: "30px", textAlign: "center",color:'white' }}>Hello <b>{LogInUser}</b> 
@@ -113,76 +127,97 @@ function Home({allUser,setAllUser}) {
             </Button>
         </h1>
 
-        <div className="cards" style={{ display: "flex", flexFlow: "row wrap", justifyContent:"center"}}>
+        <div className="cards" style={{ display: "flex", flexFlow: "row wrap"}}>
             <AnotherCard  allUser={allUser} setAllUser={setAllUser} update={update}/>
         </div>
 
         <br />
-        <Grid item container xs={10} sm={8} md={6} lg={10} style={{width: "38%", margin: "auto"}}> 
+        <Grid id="form" item container xs={10} sm={8} md={6} lg={10} style={{width: "38%", margin: "auto"}}> 
 
-        <Paper id="form"
-          elevation={12}
-          component="form"
-          sx={{
-              "& > :not(style)": { m: 1, width: "96%"},
-          }}
-          style={{padding:"10px"}}
-          noValidate
-          autoComplete="off">
+          <Paper id="form"
+            elevation={12}
+            component="form"
+            sx={{
+                "& > :not(style)": { m: 1, width: "96%"},
+            }}
+            style={{padding:"20px", border: "15px solid", borderColor:"#006bb9 #30A0E0 #FFC973 #FFE3B3", borderRadius:"15px" }}
+            noValidate
+            autoComplete="off">
 
-          <TextField id="outlined-basic"
-              required
-              value={formik.values.name}
-              label="Name"
-              variant="outlined"
-              name="name"
-              onChange={formik.handleChange}
-            />
-           <div>{formik.errors.name}</div>
-          
-          <TextField id="filled-basic"
-              required
-              value={formik.values.password}
-              label="Password"
-              type="password"
-              variant="outlined"
-              name="password"
-              onChange={formik.handleChange}
-            />
-            <div>{formik.errors.password}</div>
-
-          <strong>Available on :</strong>
-          <Grid style={{display:"flex", flexFlow:"row wrap", justifyContent:"space-around"}}>
-              <FormControlLabel
-                  control={<Checkbox id="monday" checked={formik.values.days.monday} name="days.monday" onClick={formik.handleChange} /> }
-                  label="Monday"
+            <h1 style={{textAlign:"center", fontSize:"xx-large"}}>{formik.values.id===0?"Add new user":"Update existing user"}</h1>
+            
+            <TextField id="outlined-basic"
+                required
+                value={formik.values.name}
+                label="Name"
+                variant="outlined"
+                name="name"
+                onChange={formik.handleChange}
               />
-              <FormControlLabel
-                  control={<Checkbox id="tuesday" checked={formik.values.days.tuesday} name="days.tuesday" onClick={formik.handleChange} /> }
-                  label="Tuesday"
-              />
-              <FormControlLabel
-                  control={ <Checkbox id="wednesday" checked={formik.values.days.wednesday} name="days.wednesday" onClick={formik.handleChange} /> }
-                  label="Wednesday"
-              />
-          </Grid>
-          
-          <Button
-              variant="contained"
-              // onClick={formik.handleSubmit}
-              onClick={formik.handleSubmit}>	
-              {formik.values.id===0?"Submit":"update"}
-          </Button>
-          <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '25%' }}>
-                Data successfully added
-              </Alert>
-          </Snackbar>
+            <div style={{color:"red"}}>{formik.errors.name}</div>
+            
+            <OutlinedInput value={formik.values.password} onChange={formik.handleChange} name="password" fullWidth required type={visibility?"text":"password"}  placeholder="Password *"
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton  onClick={handleshow}>
+                    {visibility?<Visibility/>:<VisibilityOffIcon/>}
+                  </IconButton>
+                </InputAdornment>
+              } />
+            <div style={{color:"red"}}>{formik.errors.password}</div>
 
-      </Paper>
+            <strong>Available on :</strong>
+            <Grid style={{display:"flex", flexFlow:"row wrap", justifyContent:"space-around"}}>
+                <FormControlLabel
+                    control={<Checkbox id="monday" checked={formik.values.days.monday} name="days.monday" onClick={formik.handleChange} /> }
+                    label="Monday"
+                />
+                <FormControlLabel
+                    control={<Checkbox id="tuesday" checked={formik.values.days.tuesday} name="days.tuesday" onClick={formik.handleChange} /> }
+                    label="Tuesday"
+                />
+                <FormControlLabel
+                    control={ <Checkbox id="wednesday" checked={formik.values.days.wednesday} name="days.wednesday" onClick={formik.handleChange} /> }
+                    label="Wednesday"
+                />
+                <FormControlLabel
+                    control={ <Checkbox id="thursday" checked={formik.values.days.thursday} name="days.thursday" onClick={formik.handleChange} /> }
+                    label="Thursday"
+                />
+                <FormControlLabel
+                    control={ <Checkbox id="friday" checked={formik.values.days.friday} name="days.friday" onClick={formik.handleChange} /> }
+                    label="Friday"
+                />
+                <FormControlLabel
+                    control={ <Checkbox id="saturday" checked={formik.values.days.saturday} name="days.saturday" onClick={formik.handleChange} /> }
+                    label="Saturday"
+                />
+                <FormControlLabel
+                    control={ <Checkbox id="sunday" checked={formik.values.days.sunday} name="days.sunday" onClick={formik.handleChange} /> }
+                    label="Sunday"
+                />
+            </Grid>
 
-  </Grid>	
-  <br />
+            <ButtonGroup variant="outlined" fullWidth>
+              <Button
+                  color="success"                
+                  // onClick={formik.handleSubmit}
+                  onClick={formik.handleSubmit}>	
+                  {formik.values.id===0?"Submit":"Update"}
+              </Button>
+              <Button color="error" onClick={handleCancelClick}> Cancel</Button>
+            </ButtonGroup>
+
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '25%' }}>
+                  Data successfully added
+                </Alert>
+            </Snackbar>
+
+          </Paper>
+
+        </Grid>	
+        <br />
 
 </div>
   )
