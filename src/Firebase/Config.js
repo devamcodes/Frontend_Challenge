251dfb8d0f05c534c8f09fb5d0c2773a773378fd
Devam Agrawal <dev.devam.a@gmail.com>
2022-08-27 10:33:41 +0530

    -- State Update fuction improved -- Firebase cinfig file added

---
+++ src/Firebase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBgPPSEdn5gpDmo8d0UXPLzxJJr0vKN0n0",
+	authDomain: "test-bf3af.firebaseapp.com",
+	projectId: "test-bf3af",
+	storageBucket: "test-bf3af.appspot.com",
+	messagingSenderId: "1050002024586",
+	appId: "1:1050002024586:web:8c4b3225c2031516dd6573",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
